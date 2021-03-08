9c82c3d18e43bc85170d70dc7ef779ae31e10836
ShaneRandell <77622184+ShaneRandell@users.noreply.github.com>
2021-03-08 09:17:43 -0400

    Update Midterm.ino

--- Midterm.ino
+++ Midterm.ino
@@ -1,3 +1,7 @@
+/* this is the code to go with python code for part 5 of midterm exam */
+
+// Delcaring all my varaibles and pin i will use in the function
+
 int LedPin8 = 8;
 int LedPin9 = 9; 
 int LedPin10 = 10; 
@@ -15,68 +19,72 @@ int button_val1;
 int button_val2;
 int button_val3;
 
+/*     Setting up all my pins as inputs and outputs to control various components      */
 
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(LedPin8, OUTPUT);
+  pinMode(LedPin8, OUTPUT);    //Setting my LED pins as outputs 
   pinMode(LedPin9, OUTPUT);
   pinMode(LedPin10, OUTPUT);
   pinMode(LedPin11, OUTPUT);
-  pinMode(Button1, INPUT);
+  pinMode(Button1, INPUT);    // Setting my Buttos pins as inputs
   pinMode(Button2, INPUT);
   pinMode(Button3, INPUT);
   pinMode(Button4, INPUT);
   
 }
 
+/* the main loop that will run all my code and functions */
+
 void loop() 
 {
-  if (Serial.available()==0)
+  if (Serial.available()==0)        // Waiting for the serial monitor to recieve information from the program
   {
-    String data = Serial.readStringUntil('\n');
-    if (data == "Pin8") // Toggle Pin8 to turn on
+    String data = Serial.readStringUntil('\n');   // reading the string printed to the serial monitor 
+    if (data == "Pin8") // reading and comparing the string in the monitor  
     {
-      digitalWrite(LedPin8, HIGH);
-      delay(2000);
+      digitalWrite(LedPin8, HIGH); // if true Toggle Pin8 to turn on for 2 seconds
+      delay(2000);                
       digitalWrite(LedPin8, LOW);
       
     }
-    else if (data == "Pin9") 
+    else if (data == "Pin9")   //else if the string reads Pin9 to toggle this led on for 2 seconds
     {
       digitalWrite(LedPin9, HIGH);
       delay(2000);
       digitalWrite(LedPin9, LOW);
       
     }
-    else if (data == "Pin10") 
+    else if (data == "Pin10") //else if the string reads Pin10 to toggle this led on for 2 seconds
     {
       digitalWrite(LedPin10, HIGH);
       delay(2000);
       digitalWrite(LedPin10, LOW);
       
     }
-    else if (data == "Pin11") 
+    else if (data == "Pin11") //else if the string reads Pin11 to toggle this led on for 2 seconds
     {
       digitalWrite(LedPin11, HIGH);
       delay(2000);
       digitalWrite(LedPin11, LOW);
       
     }
-    else
+    /* This code will run and print to the serial monitor in a specific format.
+    This will allow the python program to collect the information and parse it
+    apart and store the correct values in the correct location */
+    else    // while no if statement is triggered the following code will contiunally loop
     {
-      Pot1 = analogRead(A0);
+      Pot1 = analogRead(A0);       // Storing analog values in variables 
       Pot2 = analogRead(A1);
       Pot3 = analogRead(A2);
       Pot4 = analogRead(A3);
-      button_val = digitalRead(Button1);
+      button_val = digitalRead(Button1);  // storing the value of digital pins in vars
       button_val1 = digitalRead(Button2);
       button_val2 = digitalRead(Button3);
       button_val3 = digitalRead(Button4);
-      digitalRead(Button2);
-      digitalRead(Button3);
-      digitalRead(Button4);
-      Serial.print(Pot1);
+      //all the print function below are seperated but a "," for the python program to read it correctly
+      Serial.print(Pot1);        
       Serial.print(",");
       Serial.print(Pot2);
       Serial.print(",");
@@ -90,6 +98,7 @@ void loop()
       Serial.print(",");
       Serial.print(button_val2);
       Serial.print(",");
+      // printing the final value as a line so the next set of info in a new line
       Serial.println(button_val3);
       Serial.flush();
     }
